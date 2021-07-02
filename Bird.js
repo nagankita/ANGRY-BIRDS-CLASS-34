adf0c51b935e29180543d7df102b225a0b9246a4
nagankita <83650101+nagankita@users.noreply.github.com>
2021-07-02 18:28:21 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,18 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/bird.png");
+    this.trajectory=[]
+    this.smokeImg=loadImage("sprites/smoke.png")
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+    super.display();
+    if(this.body.position.x>200){
+    this.trajectory.push([this.body.position.x,this.body.position.y])}
+    for(var i=0;i<this.trajectory.length;i++)
+    image(this.smokeImg,this.trajectory[i][0],this.trajectory[i][1])
+  }
+}
